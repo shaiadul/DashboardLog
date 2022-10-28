ae5d76d0f2edd2a6dce18fb504449b5b07dc8113
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2022-10-28 14:03:52 +0600

    through code

---
+++ src/pages/Blank.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blank = () => {
+    return (
+        <div>
+            <img className='Blank_img' src="https://static.vecteezy.com/system/resources/previews/007/098/083/original/coming-soon-lettering-promotion-or-announcement-banner-modern-calligraphy-vector.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Blank
